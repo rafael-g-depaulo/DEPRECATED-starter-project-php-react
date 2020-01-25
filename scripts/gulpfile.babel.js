98840bbda93c2de3522ee676afb493bc749f32d1
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2020-01-25 20:53:04 -0300

    ➕ Add gulp

---
+++ scripts/gulpfile.babel.js
@@ -0,0 +1,8 @@
+import gulp from "gulp"
+
+// this is a gulpfile. it has tasks that can be run from the commandline with "gulp [task]"
+// if no task is provided, the default task will be executed
+
+
+// default task
+export default done => done()
